27791747b0d611620bbaa1bdf4fe2b052308ab03
Mike Matera <matera@lifealgorithmic.com>
2016-08-16 13:38:35 -0700

    Added basic code template.

---
+++ Template/Template.ino
@@ -0,0 +1,19 @@
+/*
+ * This is the CS-11M program template. Change this text to 
+ * be the title of the assignment and a short description 
+ * of what it does. 
+ *  
+ * Author: Michael Matera
+ * Date: 8/16/2016
+ * 
+ */
+
+void setup() {
+  
+}
+
+
+void loop() {
+  
+}
+
